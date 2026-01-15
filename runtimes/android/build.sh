bb5186c9b22169f26fdb57f19ec180057cecbca0
Tyler Nijmeh <tylernij@gmail.com>
2026-01-15 11:24:17 -0600

    android: Proper calc of download size
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- runtimes/android/build.sh
+++ runtimes/android/build.sh
@@ -6,26 +6,53 @@ build() {
     local flavor="$1"
 
     pushd "$SCRIPT_DIR/app" > /dev/null || exit 1
-    ./gradlew :app:assemble${flavor}Release
+    ./gradlew :app:bundle${flavor}Release
     popd > /dev/null
 }
 
-getsize() {
-    local runtime="$1"
-    local platform="$2"
-    local pre_app="$SCRIPT_DIR/app/app/build/outputs/apk/default/release/app-default-release-unsigned.apk"
-    local post_app="$SCRIPT_DIR/app/app/build/outputs/apk/rive/release/app-rive-release-unsigned.apk"
+split_bundle() {
+    local flavor="$1"
+    local json="$2"
+    local name="$3"
 
-    compare_size "$pre_app" "$post_app"
-    record_size "$runtime" "$platform" ""
+    local bundle="$SCRIPT_DIR/app/app/build/outputs/bundle/${flavor}Release/app-$flavor-release.aab"
+    bundletool build-apks --bundle="$bundle" --output="$bundle.$name.apks" --device-spec="$json"
+}
+
+get_download_size() {
+    local flavor="$1"
+    local json="$2"
+    local name="$3"
+
+    local apks="$SCRIPT_DIR/app/app/build/outputs/bundle/${flavor}Release/app-$flavor-release.aab.$name.apks"
+    local size=$(bundletool get-size total --apks="$apks" --device-spec="$json" --dimensions=ALL)
+    echo "$size" | awk -F ',' '{ print $13 }' | tail -1 | tr -d '[:space:]'
 }
 
 run_build() {
     local runtime="$1"
     local platform="$2"
 
-    build default
-    build rive
-    
-    getsize "$runtime" "$platform"
+    # pushd "$SCRIPT_DIR/app" > /dev/null || exit 1
+    # ./gradlew clean
+    # popd > /dev/null
+
+    # build default
+    # build rive
+
+    split_bundle default "$SCRIPT_DIR/device_armv7.json" armv7
+    split_bundle rive "$SCRIPT_DIR/device_armv7.json" armv7
+    local dl_size_default_armv7=$(get_download_size default "$SCRIPT_DIR/device_armv7.json" armv7)
+    local dl_size_rive_armv7=$(get_download_size rive "$SCRIPT_DIR/device_armv7.json" armv7)
+
+    compare_size_raw "$dl_size_default_armv7" "$dl_size_rive_armv7"
+    record_size "$runtime" "$platform" "armv7"
+
+    split_bundle default "$SCRIPT_DIR/device_armv8.json" armv8
+    split_bundle rive "$SCRIPT_DIR/device_armv8.json" armv8
+    local dl_size_default_armv8=$(get_download_size default "$SCRIPT_DIR/device_armv8.json" armv8)
+    local dl_size_rive_armv8=$(get_download_size rive "$SCRIPT_DIR/device_armv8.json" armv8)
+
+    compare_size_raw "$dl_size_default_armv8" "$dl_size_rive_armv8"
+    record_size "$runtime" "$platform" "armv8"
 }
